27477cafd4c6a62ac45300b45d4c2b907706a316
Naveen <cnaveen508@gmail.com>
2025-05-04 19:47:59 +0530

    Update vite.config.js for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // or '/' if deploying at root
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
